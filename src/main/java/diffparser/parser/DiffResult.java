4d5bab4a8994033c16d95524edf94a1bcb2cc86c
albf <albf.unicamp@gmail.com>
2018-03-10 03:31:05 -0500

    DiffApplier: Initial apply patch version
    Receives a filepath and a patch (already splitted by the split
    function), returns both original and modified versions.

---
+++ src/main/java/diffparser/parser/DiffResult.java
@@ -0,0 +1,40 @@
+package main.java.diffparser.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiffResult {
+    private String original;
+    private String modified;
+    private List<Integer> modifiedLines;
+
+    public DiffResult(String original, String modified) {
+        this.original = original;
+        this.modified = modified;
+        this.modifiedLines = new ArrayList<>();
+    }
+
+    public String getOriginal() {
+        return original;
+    }
+
+    public void setOriginal(String original) {
+        this.original = original;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public void setModified(String modified) {
+        this.modified = modified;
+    }
+
+    public List<Integer> getModifiedLines() {
+        return modifiedLines;
+    }
+
+    public void setModifiedLines(List<Integer> modifiedLines) {
+        this.modifiedLines = modifiedLines;
+    }
+}
