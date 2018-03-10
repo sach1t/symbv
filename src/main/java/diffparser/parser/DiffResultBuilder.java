f4f1fb703029692d42e358cf3773403cb4ae01eb
albf <albf.unicamp@gmail.com>
2018-03-10 18:07:03 -0500

    DiffApplier: remove diff_match_patch
    diff_match_path doesn't work. It doesn't apply git hunks correctly, it's
    too buggy, just use a manually made parser instead.
    
    Tests are passing now, still needs to identify modified lines.

---
+++ src/main/java/diffparser/parser/DiffResultBuilder.java
@@ -0,0 +1,28 @@
+package main.java.diffparser.parser;
+
+import java.util.List;
+
+public class DiffResultBuilder {
+    private String original;
+    private String modified;
+    private List<Integer> modifiedLines;
+
+    public DiffResultBuilder setOriginal(String original) {
+        this.original = original;
+        return this;
+    }
+
+    public DiffResultBuilder setModified(String modified) {
+        this.modified = modified;
+        return this;
+    }
+
+    public DiffResultBuilder setModifiedLines(List<Integer> modifiedLines) {
+        this.modifiedLines = modifiedLines;
+        return this;
+    }
+
+    public DiffResult createDiffResult() {
+        return new DiffResult(original, modified, modifiedLines);
+    }
+}
