eef79942ae1bec69723b8f3a074f6e3d7021a517
albf <albf.unicamp@gmail.com>
2018-03-10 00:35:31 -0500

    DiffApplier: Fix filepath and add new test
    Filepaths shouldn't include initial "/". Also, add a test with two files
    modified test.

--- src/test/main/java/diffparser/parser/DiffApplierTest.java
+++ src/test/main/java/diffparser/parser/DiffApplierTest.java
@@ -32,13 +32,59 @@ public class DiffApplierTest {
             "-ddddddd\n" +
             "+ddddd";
 
+    String twoChangesPatch = "From 397ff5d8cc1091852dfec56d42b1b472f15416db Mon Sep 17 00:00:00 2001\n" +
+            "From: albf <albf.unicamp@gmail.com>\n" +
+            "Date: Sat, 10 Mar 2018 00:30:05 -0500\n" +
+            "Subject: [PATCH] two files modification\n" +
+            "\n" +
+            "---\n" +
+            " src/main/java/diffparser/parser/x | 3 +--\n" +
+            " src/main/java/diffparser/parser/y | 2 +-\n" +
+            " 2 files changed, 2 insertions(+), 3 deletions(-)\n" +
+            "\n" +
+            "diff --git a/src/main/java/diffparser/parser/x b/src/main/java/diffparser/parser/x\n" +
+            "index 9405325..50d4abb 100644\n" +
+            "--- a/src/main/java/diffparser/parser/x\n" +
+            "+++ b/src/main/java/diffparser/parser/x\n" +
+            "@@ -1,5 +1,4 @@\n" +
+            " a\n" +
+            "-b\n" +
+            "+k\n" +
+            " c\n" +
+            "-d\n" +
+            " e\n" +
+            "diff --git a/src/main/java/diffparser/parser/y b/src/main/java/diffparser/parser/y\n" +
+            "index 8baef1b..24c5735 100644\n" +
+            "--- a/src/main/java/diffparser/parser/y\n" +
+            "+++ b/src/main/java/diffparser/parser/y\n" +
+            "@@ -1 +1 @@\n" +
+            "-abc\n" +
+            "+def\n" +
+            "-- \n" +
+            "2.11.0\n" +
+            "\n";
+
+    String[] twoChangesPatchExpected = {
+            "\n@@ -1,5 +1,4 @@\n" +
+                    " a\n" +
+                    "-b\n" +
+                    "+k\n" +
+                    " c\n" +
+                    "-d\n" +
+                    " e",
+
+            "\n@@ -1 +1 @@\n" +
+                    "-abc\n" +
+                    "+def"
+    };
+
     @Test
     public void shouldSplitGitDiffWithOneChange() {
         Map<String, String> patches = diffApplier.splitGitPatch(oneChangePatch);
 
         Assert.assertEquals(1, patches.size());
-        Assert.assertEquals(true, patches.containsKey("/src/test/x"));
-        Assert.assertEquals(oneChangePatchExpected, patches.get("/src/test/x"));
+        Assert.assertEquals(true, patches.containsKey("src/test/x"));
+        Assert.assertEquals(oneChangePatchExpected, patches.get("src/test/x"));
     }
 
     @Test
@@ -47,5 +93,18 @@ public class DiffApplierTest {
         Map<String, String> patches = diffApplier.splitGitPatch(newFilePatch);
 
         Assert.assertEquals(0, patches.size());
+    }
+
+    @Test
+    public void shouldSplitGitDiffWithTwoChanges() {
+        Map<String, String> patches = diffApplier.splitGitPatch(twoChangesPatch);
+
+        Assert.assertEquals(2, patches.size());
+
+        Assert.assertEquals(true, patches.containsKey("src/main/java/diffparser/parser/x"));
+        Assert.assertEquals(twoChangesPatchExpected[0], patches.get("src/main/java/diffparser/parser/x"));
+
+        Assert.assertEquals(true, patches.containsKey("src/main/java/diffparser/parser/y"));
+        Assert.assertEquals(twoChangesPatchExpected[1], patches.get("src/main/java/diffparser/parser/y"));
     }
 }
