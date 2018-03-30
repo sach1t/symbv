703299ca07ef27a83a0a62471a2aea9d6e156bfb
sachit <sach0010t@gmail.com>
2018-03-30 00:21:49 -0400

    Add simple example

---
+++ src/examples/simple/Runner.java
@@ -0,0 +1,21 @@
+package simple;
+
+public class Runner {
+
+    public static void main(String args[]) {
+        Runner r = new Runner();
+        r.test(0,0,0);
+    }
+
+    public void test(int pedalPos, int bSwitch, int pedalCmd) {
+        Test original = new Test();
+        TestPatched patched = new TestPatched();
+
+        IntPair ret = original.update(pedalPos, bSwitch, pedalCmd);
+        IntPair ret2 = patched.update(pedalPos, bSwitch, pedalCmd);
+
+        if (ret.equals(ret2)) {
+            throw new Error();
+        }
+    }
+}
