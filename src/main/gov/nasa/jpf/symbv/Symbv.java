52866c4c8a78e6252dcbb202d2e03644cfdc5843
sachit <sach0010t@gmail.com>
2018-03-31 16:41:41 -0400

    Add basic implementation

--- src/main/gov/nasa/jpf/symbv/Symbv.java
+++ src/main/gov/nasa/jpf/symbv/Symbv.java
@@ -8,21 +8,30 @@ import gov.nasa.jpf.util.LogManager;
 
 
 public class Symbv implements JPFShell {
-	private Config config;
-	private JPFLogger logger;
+    private Config config;
+    private JPFLogger logger;
 
-	public Symbv(Config conf) {
-		this.config = conf;
-		LogManager.init(conf);
-		logger = JPF.getLogger("symbv");
-	}
+    public Symbv(Config conf) {
+        this.config = conf;
+        LogManager.init(conf);
+        logger = JPF.getLogger("symbv");
+    }
 
-	@Override
-	public void start(String[] args) {
-		run();
-	}
+    @Override
+    public void start(String[] args) {
+        // this information can be gotten using JavaParser
+        String target = "simple.Runner"; // fully qualified class name
+        String methodSpec = "run(i: int, j: int, k: int)"; // run method
 
-	public void run() {
-		System.out.println("It Works!");
-	}
+        SymbvConfig sConf = new SymbvConfig(config);
+        sConf.setConcolic(target, methodSpec);
+        sConf.setMethodInstanceFieldsSymbolic();
+        run(sConf);
+    }
+
+    public void run(SymbvConfig sconf) {
+        Executor ex = new Executor(sconf);
+        ExecutionResult er = ex.run();
+        er.printOkayPaths();
+    }
 }
