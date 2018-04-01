d7d77a0ea27b0cf654c6917dfb1886fa8fc9db46
sachit <sach0010t@gmail.com>
2018-04-01 15:32:31 -0400

    Simple repeated concolic execution

--- src/main/gov/nasa/jpf/symbv/Symbv.java
+++ src/main/gov/nasa/jpf/symbv/Symbv.java
@@ -7,7 +7,7 @@ import gov.nasa.jpf.util.JPFLogger;
 import gov.nasa.jpf.util.LogManager;
 
 
-public class Symbv implements JPFShell {
+public class  Symbv implements JPFShell {
     private Config config;
     private JPFLogger logger;
 
@@ -20,18 +20,17 @@ public class Symbv implements JPFShell {
     @Override
     public void start(String[] args) {
         // this information can be gotten using JavaParser
-        String target = "simple.Runner"; // fully qualified class name
-        String methodSpec = "run(i: int, j: int, k: int)"; // run method
+        String target = "simple2.Runner"; // fully qualified class name
+        String methodSpec = "run(i: int)"; // run method
 
         SymbvConfig sConf = new SymbvConfig(config);
-        sConf.setConcolic(target, methodSpec);
-        sConf.setMethodInstanceFieldsSymbolic();
+        sConf.setConcolicMethod(target, methodSpec);
+        sConf.setInstanceFieldsSymbolic();
         run(sConf);
     }
 
     public void run(SymbvConfig sconf) {
         Executor ex = new Executor(sconf);
         ExecutionResult er = ex.run();
-        er.printOkayPaths();
     }
 }
