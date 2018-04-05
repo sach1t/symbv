1aaf1bff41513f83b23861c968325eb7d81ea09f
albf <albf.unicamp@gmail.com>
2018-04-05 13:28:55 -0400

    CodeExplorer: add CodeMethod
    Add a way to extract and insert method between classes.

--- src/main/java/diffparser/parser/CodeExplorer.java
+++ src/main/java/diffparser/parser/CodeExplorer.java
@@ -2,7 +2,10 @@ package main.java.diffparser.parser;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.expr.SimpleName;
 
 import java.util.*;
@@ -49,6 +52,58 @@ public class CodeExplorer {
                 }));
 
         return modifiedMethods;
+    }
+
+    private TypeDeclaration findClass(String className) throws Exception {
+        for (int i = 0; i < this.cu.getTypes().size(); i++) {
+            TypeDeclaration type = this.cu.getTypes().get(i);
+            if (type.getNameAsString().equals(className)) {
+                return type;
+            }
+        }
+
+        throw new Exception("Can't find class: " + className);
+    }
+
+    private String[] splitCompleteName(String completeName) throws Exception {
+        String[] parts = completeName.split("\\.");
+        if (parts.length < 2) {
+            throw new Error("Should receive at least class and method names");
+        }
+        return parts;
+    }
+
+    public void includeMethod(String completeName, CodeMethod codeMethod) throws Exception {
+        String[] parts = this.splitCompleteName(completeName);
+
+        String className = parts[parts.length-2];
+        String methodName = parts[parts.length-1];
+
+        TypeDeclaration type = this.findClass(className);
+        EnumSet<Modifier> modifiers = codeMethod.getModifiers();
+        MethodDeclaration methodDeclaration = type.addMethod(methodName, modifiers.toArray(new Modifier[modifiers.size()]));
+
+        if (codeMethod.getBlock().isPresent()) {
+            methodDeclaration.setBody(codeMethod.getBlock().get());
+        }
+    }
+
+    public CodeMethod findCodeMethod(String completeName) throws Exception {
+        String [] parts = this.splitCompleteName(completeName);
+
+        String className = parts[parts.length-2];
+        String methodName = parts[parts.length-1];
+
+        TypeDeclaration type = this.findClass(className);
+
+        List<MethodDeclaration> declarations = type.getMethodsByName(methodName);
+        if (declarations.size() == 0) {
+            throw new Error("Can't find method: " + methodName + " on class: " + className);
+        }
+
+        // TODO: Deal with overloads
+        MethodDeclaration methodDeclaration = declarations.get(0);
+        return new CodeMethod(methodDeclaration.getModifiers(), methodDeclaration.getBody());
     }
 
     public void replaceClassNames() {
