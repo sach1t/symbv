e1716f00e433b8ee91eed2ef3ffd713e1541fcb6
albf <albf.unicamp@gmail.com>
2018-04-05 23:01:27 -0400

    TestGenerator: genRunFunction
    Add a method to generate the actual tested function.

--- src/test/main/java/diffparser/parser/TestGeneratorTest.java
+++ src/test/main/java/diffparser/parser/TestGeneratorTest.java
@@ -16,7 +16,7 @@ public class TestGeneratorTest {
     }
 
     @Test
-    public void shouldGenerateTestFunctionCorrectly() throws Exception {
+    public void shouldGenerateMainFunctionCorrectly() throws Exception {
         List<Pair<String, String>> args = Arrays.asList(new Pair("int", "arg1"), new Pair("boolean", "arg2"), new Pair("otherClass", "arg3"));
         CodeMethod codeMethod = new CodeMethod("aaa.b.c", args, "NOT_NEEDED", null, null);
         TestGenerator testGenerator = new TestGenerator(codeMethod, false);
@@ -26,5 +26,26 @@ public class TestGeneratorTest {
                 + "t.run(0, true, new otherClass()); }";
 
         Assert.assertEquals(noSpacings(expected), noSpacings(testGenerator.genMainFunction()));
+    }
+
+    @Test
+    public void shouldGenerateRunFunctionCorrectly() throws Exception {
+        List<Pair<String, String>> args = Arrays.asList(new Pair("int", "arg1"), new Pair("boolean", "arg2"), new Pair("otherClass", "arg3"));
+        CodeMethod codeMethod = new CodeMethod("aaa.b.c", args, "int", null, null);
+        codeMethod.setOriginalName("c___original");
+        TestGenerator testGenerator = new TestGenerator(codeMethod, false);
+
+        String expected = "public void run(int arg1, boolean arg2, otherClass arg3) {\n "
+                + "b original = new b();"
+                + "b patched = new b();"
+
+                + "int originalResult = original.c___original(arg1, arg2, arg3);"
+                + "int patchedResult = patched.c(arg1, arg2, arg3);"
+
+                + "if (originalResult == patchedResult) {"
+                + "  throw new Error();"
+                + "}}";
+
+        Assert.assertEquals(noSpacings(expected), noSpacings(testGenerator.genRunFunction()));
     }
 }
