ae6d64627348ee90a22529cb186cba0f2ba33fdb
albf <albf.unicamp@gmail.com>
2018-04-07 01:44:02 -0400

    Arguments: Add simple arguments to define step
    Step 1: "gen" -> generate
    Step 2: "exec" -> execute tests
    
    Also use a base path for all IO operations, since it's only used by
    diffparser and everything there should be related to the patch path.

--- src/main/diffparser/io/FileManager.java
+++ src/main/diffparser/io/FileManager.java
@@ -5,20 +5,47 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileManager {
+    private String basePath;
+
+    public FileManager() {
+        this.useCurrentDirectory();
+    }
+
+    private void useCurrentDirectory() {
+        this.basePath = Paths.get(".").toAbsolutePath().normalize().toString();
+    }
+
+    public FileManager(String patchPath) {
+        File patchFile = new File(patchPath);
+        if (!patchFile.exists() || !patchFile.isFile()) {
+            System.out.println("Bad patchPath provided");
+            this.useCurrentDirectory();
+            return;
+        }
+
+        // use parent directory as basePath
+        this.basePath = patchFile.getAbsoluteFile().getParent();
+    }
+
+    private Path joinWithBase(String filepath) {
+        return Paths.get(this.basePath, filepath);
+    }
+
     public String readFile(String filepath) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(filepath)));
+        return new String(Files.readAllBytes(this.joinWithBase(filepath)));
     }
 
     public void writeFile(String filepath, String content) throws FileNotFoundException {
-        PrintWriter out = new PrintWriter(filepath);
+        PrintWriter out = new PrintWriter(this.joinWithBase(filepath).toFile());
         out.print(content);
     }
 
     public void createDirectory(String dirpath) {
-        File directory = new File(dirpath);
+        File directory = this.joinWithBase(dirpath).toFile();
         if (!directory.exists()){
             directory.mkdir();
         }
