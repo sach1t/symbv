aad5741a8f6a511377c680da1f294841a52d7e5b
albf <albf.unicamp@gmail.com>
2018-04-07 02:09:10 -0400

    genBasic example added
    Adds a simple example to exercise test generation. It already includes a
    patch, required for part1.

---
+++ src/examples/genBasic/math.java
@@ -0,0 +1,12 @@
+package genBasic;
+
+public class math {
+    public int fibonacci(int n)  {
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
