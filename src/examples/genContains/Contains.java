2abcbce0e120f11bb6f95a02afc67423a335a38c
albf <albf.unicamp@gmail.com>
2018-04-08 02:34:19 -0400

    genContains: Add new multiple modification example

--- src/examples/genContains/Contains.java
+++ src/examples/genContains/Contains.java
@@ -1,4 +1,14 @@
 package genContains;
 
 public class Contains {
+    public boolean contains(String text, String query) {
+        // Count the size of a split. Dumb approach.
+        return text.split(query, -1).length > 2;
+    }
+
+    public boolean containsAny(String text, String query1, String query2) {
+        // Another dumb mistake. Any should be "OR", but it's "AND" here.
+        return contains(text, query1) &&
+                contains(text, query2);
+    }
 }
