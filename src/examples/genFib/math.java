f047ace4a3e44ac3023cc6c748ab30d9e77e40e0
sachit <sach0010t@gmail.com>
2018-04-09 19:48:48 -0400

    Negative numbers are not valid

--- src/examples/genFib/math.java
+++ src/examples/genFib/math.java
@@ -2,7 +2,7 @@ package genBasic;
 
 public class math {
     public int fibonacci(int n)  {
-        if(n == 0)
+        if(n <= 0)
             return 0;
         else if(n == 1)
             return 1;
