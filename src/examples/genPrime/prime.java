1ededd031fe17e15052510739ae700b2fbcf8aa4
sachit <sach0010t@gmail.com>
2018-04-09 19:42:57 -0400

    Underflow leads too expensive computation

--- src/examples/genPrime/prime.java
+++ src/examples/genPrime/prime.java
@@ -2,7 +2,7 @@ package genPrime;
 
 public class prime {
     public boolean isPrime(int value) {
-        for (int i = value-1; i > 1; i--) {
+        for (int i = value; i > 1; i--) {
             if ((value % i) == 0) {
                 return false;
             }
