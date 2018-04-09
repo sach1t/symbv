d2cebaa563655339c6549479ac4cc9076552a793
sachit <sach0010t@gmail.com>
2018-04-09 17:50:37 -0400

    Enable getting error paths from execution result

--- src/main/gov/nasa/jpf/symbv/ExecutionResult.java
+++ src/main/gov/nasa/jpf/symbv/ExecutionResult.java
@@ -2,10 +2,13 @@ package gov.nasa.jpf.symbv;
 
 import gov.nasa.jpf.constraints.api.Valuation;
 import gov.nasa.jpf.jdart.ConcolicExplorer;
+import gov.nasa.jpf.jdart.constraints.ConstraintsTree;
 import gov.nasa.jpf.jdart.constraints.Path;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
 
 public class ExecutionResult {
 
@@ -15,17 +18,25 @@ public class ExecutionResult {
         this.ce = ce;
     }
 
-    public List<Path> getOkayPaths() {
+    public List<Path> getPaths(Function<ConstraintsTree, Collection<Path>> pathType) {
         List<Path> paths = new ArrayList<>();
 
         ce.getCompletedAnalyses().forEach((methodName, completedAnalyses) ->
                 completedAnalyses.forEach(completedAnalysis -> {
                     if (completedAnalysis.getConstraintsTree() != null) {
-                        paths.addAll(completedAnalysis.getConstraintsTree().getCoveredPaths());
+                        paths.addAll(pathType.apply(completedAnalysis.getConstraintsTree()));
                     }
                 }));
 
         return paths;
+    }
+
+    public List<Path> getOkayPaths() {
+        return getPaths(ConstraintsTree::getCoveredPaths);
+    }
+
+    public List<Path> getErrorPaths() {
+        return getPaths(ConstraintsTree::getErrorPaths);
     }
 
     public String toString () {
